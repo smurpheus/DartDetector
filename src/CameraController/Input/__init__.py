20ee125bc427daf6f9acbbac519d84e143f9c0e3
mhertzer@tzi.de <mhertzer@tzi.de>
2018-01-11 17:34:34 +0100

    Did some Thread safety, also added a incrementativ steering controller

--- src/CameraController/Input/__init__.py
+++ src/CameraController/Input/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from threading import Thread, RLock, Event
+from threading import Thread, RLock, Event, Lock
 import pygame
 import time
 from pygame import locals
@@ -14,20 +14,16 @@ class Controller(Thread):
     def __init__(self):
         Thread.__init__(self)
         self.EVENTS = {"READY": Event()}
+        self.CALLBACKS = {}
         self.running = Event()
         self.ready = False
-        self.callbacks = {}
+        self.controller = None
+        self.buttons = 0
+        self.axes = 0
+        self.eventlock = Lock()
+        self.callbacklock = Lock()
         pygame.init()
         pygame.joystick.init()
-
-    def wait_for(self, event, timeout=None):
-        if event in self.EVENTS.keys():
-            self.EVENTS[event].wait(timeout)
-        else:
-            self.EVENTS[event] = Event()
-            self.EVENTS[event].wait(timeout)
-        self.EVENTS[event] = Event()
-        return
 
     def get_available_controller(self):
         self.reinitialize()
@@ -38,81 +34,74 @@ class Controller(Thread):
             joys.append((i, j.get_name()))
         return joys
 
+    def get_available_axes(self):
+        if self.ready:
+            return self.axes
+        return None
+
+    def get_available_buttons(self):
+        if self.ready:
+            return self.buttons
+        return None
+
     def init_controller(self, id):
         self.controller = pygame.joystick.Joystick(id)
         self.controller.init()
+        self.buttons = self.controller.get_numbuttons()
+        self.axes = self.controller.get_numaxes()
         self.ready = True
         self.emit("READY")
 
-    def register_to_event(self, event, callback):
-        if event in self.callbacks.keys():
-            self.callbacks[event] = self.callbacks[event].append(callback)
-        else:
-            self.callbacks[event] = [callback]
-
-    def emit(self, event, *args):
+    def wait_for(self, event, timeout=None):
         if event in self.EVENTS.keys():
-            self.EVENTS[event].set()
+            self.EVENTS[event].wait(timeout)
+        else:
+            with self.eventlock:
+                self.EVENTS[event] = Event()
+            self.EVENTS[event].wait(timeout)
+        with self.eventlock:
             self.EVENTS[event] = Event()
-        if event in self.callbacks.keys():
-            for fnc in self.callbacks[event]:
-                fnc(args)
-
-    def run(self):
-        buttons = self.controller.get_numbuttons()
-        axes = self.controller.get_numaxes()
-        while not self.running.is_set():
-            for e in pygame.event.get():  # iterate over event stack
-                # print('event : ' + str(e.type))
-                if e.type == pygame.locals.JOYAXISMOTION:  # 7
-                    # x, y = self.controller.get_axis(0), self.controller.get_axis(1)
-
-                    for i in range(axes):
-                        axis = self.controller.get_axis(i)
-                        self.emit("Axis{}".format(i), axis)
-
-                if e.type == pygame.locals.JOYBUTTONDOWN:
-                    for i in range(buttons):
-                        button = self.controller.get_button(i)
-                        if button > 0:
-                            self.emit("Button{}".format(i), button)
-                        # print("Axis {} value: {:>6.3f}".format(i, axis))
-                    # print('x and y : ' + str(x) + ' , ' + str(y))
-                # elif e.type == pygame.locals.JOYBALLMOTION:  # 8
-                #     print('ball motion')
-                # elif e.type == pygame.locals.JOYHATMOTION:  # 9
-                #     print('hat motion')
-                # elif e.type == pygame.locals.JOYBUTTONDOWN:  # 10
-                #     print('button down')
-                # elif e.type == pygame.locals.JOYBUTTONUP:  # 11
-                #     print('button up')
+        return
 
     def reinitialize(self):
         pygame.quit()
         pygame.init()
         pygame.joystick.init()
 
+    def register_to_event(self, event, callback):
+        with self.callbacklock:
+            if event in self.CALLBACKS.keys():
+                self.CALLBACKS[event] = self.CALLBACKS[event].append(callback)
+            else:
+                self.CALLBACKS[event] = [callback]
 
-def printvalue(result):
-    print("X{}".format(result))
+    def emit(self, event, *args):
+        with self.eventlock:
+            if event in self.EVENTS.keys():
+                self.EVENTS[event].set()
+                self.EVENTS[event] = Event()
+        with self.callbacklock:
+            if event in self.CALLBACKS.keys():
+                for fnc in self.CALLBACKS[event]:
+                    fnc(args)
 
+    def run(self):
+        while not self.running.is_set():
+            with self.eventlock, self.callbacklock:
+                watched_axes = [x for x in self.EVENTS.keys() if "Axis" in x]
+                watched_buttons = [x for x in self.EVENTS.keys() if "Button" in x]
+                watched_axes += [x for x in self.CALLBACKS.keys() if "Axis" in x]
+                watched_buttons += [x for x in self.CALLBACKS.keys() if "Button" in x]
+            for e in pygame.event.get():  # iterate over event stack
+                # print('event : ' + str(e.type))
+                if e.type == pygame.locals.JOYAXISMOTION and len(watched_axes) > 0:  # 7
+                    for a in watched_axes:
+                        i = int(a.replace("Axis", ""))
+                        if i == e.axis:
+                            self.emit("Axis{}".format(i), e.value)
 
-def printyvalue(result):
-    print("Y{}".format(result))
-
-
-def printbutton(re):
-    print("Button0{}".format(re))
-
-
-c = Controller()
-
-c.init_controller(0)
-c.register_to_event("Axis0", printvalue)
-c.register_to_event("Axis1", printyvalue)
-c.register_to_event("Button0", printbutton)
-c.start()
-c.wait_for("Button7")
-c.running.set()
-# c.emit("Axis0", 0)
-# c.do_run()
+                if e.type == pygame.locals.JOYBUTTONDOWN and len(watched_buttons) > 0:
+                    for a in watched_buttons:
+                        i = int(a.replace("Button", ""))
+                        if i == e.button:
+                            self.emit("Button{}".format(i), True)
---
+++ src/CameraController/__init__.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+from sys import stdout
+from threading import Lock, Event, Thread
+
+import time
+
+from src.CameraController.Input import Controller
+
+
+class SteeringUnit(Thread):
+    Deadzone = 0.18
+    Sensitivity = 0.5
+
+    def __init__(self, maxx=90, minx=-90, maxy=90, miny=-90):
+        Thread.__init__(self)
+        self.max_x = maxx
+        self.max_y = maxy
+        self.min_x = minx
+        self.min_y = miny
+        self.current_x = 0
+        self.current_y = 0
+        self.current_lock = Lock()
+        self.velocity_x = 0
+        self.velocity_y = 0
+        self.velocity_lock = Lock()
+        self.running = Event()
+
+    def run(self):
+        while not self.running.is_set():
+            with self.current_lock, self.velocity_lock:
+                if abs(self.velocity_x) > self.Deadzone:
+                    self.change_x(self.velocity_x * self.Sensitivity)
+                # else:
+                #     print("In Neutral X")
+                if abs(self.velocity_y) > self.Deadzone:
+                    self.change_y(self.velocity_y * self.Sensitivity)
+                stdout.write("\rX:{:02.2f}Y:{:02.2f}".format(self.current_x, self.current_y))
+                stdout.flush()
+                # else:
+                #     print("In Neutral Y")
+            time.sleep(0.0001)
+
+
+    def change_x(self, value):
+        # print("Changing by: {}".format(value))
+        if self.current_x + value < self.max_x and self.current_x + value > self.min_x:
+            self.current_x += value
+            # print("New X: {}".format(self.current_x))
+        # else:
+            # print("Maximum X reached!")
+
+    def change_y(self, value):
+        if self.current_y + value < self.max_y and self.current_y + value > self.min_y:
+            self.current_y += value
+            # print("New Y: {}".format(self.current_y))
+        # else:
+            # print("Maximum Y reached!")
+
+    def set_velocity_x(self, value):
+        value = value[0]
+        with self.velocity_lock:
+            self.velocity_x = value
+    def set_velocity_y(self, value):
+        value = value[0]
+        with self.velocity_lock:
+            self.velocity_y = value
+
+    def reset(self, *args):
+        with self.current_lock:
+            self.current_x = 0
+            self.current_y = 0
+
+# c = Controller()
+# su = SteeringUnit()
+# c.init_controller(0)
+# c.register_to_event("Axis0", su.set_velocity_x)
+# c.register_to_event("Axis1", su.set_velocity_y)
+# c.register_to_event("Button6", su.reset)
+# c.start()
+# su.start()
+# c.wait_for("Button7")
+# su.running.set()
+# c.running.set()
+# c.emit("Axis0", 0)
+# c.do_run()
